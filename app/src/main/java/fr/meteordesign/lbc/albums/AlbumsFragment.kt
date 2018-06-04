e71532ea7ff52128b57a5814763c51ac6b9cd956
Nicolas BARCACCI <nicolas.barcacci@icloud.com>
2018-06-04 21:48:53 +0200

    Create Albums Fragment

---
+++ app/src/main/java/fr/meteordesign/lbc/albums/AlbumsFragment.kt
@@ -0,0 +1,14 @@
+package fr.meteordesign.lbc.albums
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.meteordesign.lbc.R
+
+class AlbumsFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_albums, container, false)
+}
