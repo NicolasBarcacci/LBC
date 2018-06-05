6321a4a2616234dac13b0ac85eacd52ce9e68253
Nicolas BARCACCI <nicolas.barcacci.ext@parrot.com>
2018-06-05 13:13:28 +0200

    Remove Paging

--- domain/src/main/java/fr/meteordesign/domain/repository/MusicRepository.kt
+++ domain/src/main/java/fr/meteordesign/domain/repository/MusicRepository.kt
@@ -1,11 +1,10 @@
 package fr.meteordesign.domain.repository
 
 import android.arch.lifecycle.LiveData
-import android.arch.paging.PagedList
 import fr.meteordesign.domain.Album
 import fr.meteordesign.domain.Song
 
 interface MusicRepository {
-    fun albumsByPage(): LiveData<PagedList<Album>>
+    fun albumsByPage(): LiveData<List<Album>>
     fun songs(albumId: Long): LiveData<List<Song>>
 }
