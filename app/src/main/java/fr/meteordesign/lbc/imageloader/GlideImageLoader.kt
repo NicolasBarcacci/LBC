3c5eccc8d972050edd2a9e48809da819dd63fff1
Nicolas BARCACCI <nicolas.barcacci@icloud.com>
2018-06-10 17:27:28 +0200

    Display a placeholder if the app is unable to download the photo

--- app/src/main/java/fr/meteordesign/lbc/imageloader/GlideImageLoader.kt
+++ app/src/main/java/fr/meteordesign/lbc/imageloader/GlideImageLoader.kt
@@ -3,8 +3,10 @@ package fr.meteordesign.lbc.imageloader
 import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import fr.meteordesign.lbc.R
 
-class GlideImageLoader(private val context: Context) : ImageLoader{
+class GlideImageLoader(private val context: Context) : ImageLoader {
 
     private lateinit var url: String
 
@@ -16,6 +18,10 @@ class GlideImageLoader(private val context: Context) : ImageLoader{
     override fun into(imageView: ImageView) {
         Glide.with(context)
                 .load(url)
+                .apply(requestOptions())
                 .into(imageView)
     }
+
+    private fun requestOptions(): RequestOptions = RequestOptions()
+            .error(R.drawable.ic_photo_place_holder)
 }
